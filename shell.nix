84501202af5319eabfce2c803f509ff63f6bc54f
Stefan Fehrenbach <stefan.fehrenbach@gmail.com>
2019-07-13 13:21:23 +0100

    Test PureScript 0.13.2

--- shell.nix
+++ shell.nix
@@ -1,12 +1,12 @@
 let
   pkgs = import <nixpkgs> {};
-  easy-ps = import (fetchTarball https://github.com/justinwoo/easy-purescript-nix/archive/0b2c378b360ca0ff92e3fe3a41d191457036b4d6.tar.gz);
+  easy-ps = import (fetchTarball https://github.com/justinwoo/easy-purescript-nix/archive/9a8d138663c5d751e3a84f1345166e1f0f760a07.tar.gz) { inherit pkgs; };
 in pkgs.stdenv.mkDerivation {
   name = "purescript-unordered-collections-shell";
   buildInputs = [
     pkgs.nodePackages.bower
     pkgs.git # required by bower for github dependencies
     pkgs.gnumake
-    easy-ps.purescript
+    easy-ps.purs
   ];
 }
