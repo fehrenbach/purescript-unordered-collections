b95badbf28d00101911060a8c5ef2ec0f4706495
Stefan Fehrenbach <stefan.fehrenbach@gmail.com>
2020-11-15 12:35:51 +0000

    Update easy-purescript-nix, add pulp to shell

--- shell.nix
+++ shell.nix
@@ -1,12 +1,13 @@
 let
   pkgs = import <nixpkgs> {};
-  easy-ps = import (fetchTarball https://github.com/justinwoo/easy-purescript-nix/archive/9a8d138663c5d751e3a84f1345166e1f0f760a07.tar.gz) { inherit pkgs; };
+  easy-ps = import (fetchTarball https://github.com/justinwoo/easy-purescript-nix/archive/7b1c1635e16c7f12065db2f8ec049030fcc63655.tar.gz) { inherit pkgs; };
 in pkgs.stdenv.mkDerivation {
   name = "purescript-unordered-collections-shell";
   buildInputs = [
-    pkgs.nodePackages.bower
+    easy-ps.pulp
+    easy-ps.purs
     pkgs.git # required by bower for github dependencies
     pkgs.gnumake
-    easy-ps.purs
+    pkgs.nodePackages.bower
   ];
 }
